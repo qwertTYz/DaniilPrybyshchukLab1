c0de23057913d0a8dae12fee5943b0430cc76986
qwertTYz <166728293+qwertTYz@users.noreply.github.com>
2025-03-22 22:04:58 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Library
+{
+    public class Car
+    {
+        private bool VANID { get; set; }
+        public string Model { get; set; }
+
+        public string PlateNumber { get; set; }
+
+        public enum VehicleType
+        {
+            sedan,
+            hatchback
+        }
+
+        public VehicleType vehicleType { get; set; }
+
+        public Car()
+        {
+            
+        }
+
+        public Car(bool vanid, string model, string plate_number, VehicleType vehicle_type)
+        {
+            this.VANID = vanid;
+            this.Model = model;
+            this.PlateNumber = plate_number; 
+            this.vehicleType = vehicle_type;
+        }
+
+        public static Car Create(bool vanid, string model, string plate_number, VehicleType vehicle_type)
+        {
+            return new Car(vanid, model, plate_number, vehicle_type);
+        }
+
+        public void PrintObject()
+        {
+            Console.WriteLine($"vanid: {this.VANID}   model: {this.Model}   plate number: {this.PlateNumber}   vehicle type: {this.vehicleType}");
+        }
+    }
+}
---
+++ Task3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Lab1Task3;
+public class Task3
+{
+    public static void Main()
+    {
+        Assembly assembly = Assembly.LoadFrom("Lab1Library.dll");
+
+        try
+        {
+            Type type = assembly.GetType("Lab1Library.Service");
+            MethodInfo minfo = type.GetMethod("Create");
+            object typeInstance = Activator.CreateInstance(type);
+            object[] parameters = new object[minfo.GetParameters().Length];
+            parameters[0] = "servis";
+            parameters[1] = "street";
+            parameters[2] = false;
+
+
+            var temp = minfo.Invoke(typeInstance, parameters);
+
+            minfo = type.GetMethod("PrintObject");
+            minfo.Invoke(temp, null);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+        Console.ReadKey();
+    }
+}
