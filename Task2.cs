5825ba6a538f9fbe5dbdc2f75dc50c70b32c30bb
qwertTYz <166728293+qwertTYz@users.noreply.github.com>
2025-03-22 22:08:22 +0200

    Add files via upload

---
+++ Task2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Lab1Task2;
+public class Task2
+{ 
+    public static void Main()
+    {
+
+        Assembly assembly = Assembly.LoadFrom("Lab1Library.dll");
+
+        Type[] types = assembly.GetTypes();
+        foreach (Type t in types)
+        {
+            Console.WriteLine("class: " + t.Name);
+            foreach(var temp in t.GetProperties())
+            {
+                Console.WriteLine("\t\tproperty:" + temp.Name);
+            }
+            foreach(var temp in t.GetMethods())
+            {
+                Console.WriteLine("\t\tmethod:" + temp.Name);
+            }
+        }        
+
+        Console.ReadKey();
+    }
+}
